fcba9bfc852c00ab11731e701dabc830b1dd5afd
Adam Laughlin <adam.laughlin@gmail.com>
2023-01-01 16:23:36 -0700

    swap data hook to useSyncExternalStore

--- src/datagraph-util.ts
+++ src/datagraph-util.ts
@@ -1,13 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useSyncExternalStore } from 'react';
 import zcreate from 'zustand/vanilla';
+import zcreater from 'zustand';
 
 type Create = <V>(fn:()=>V) =>[ ()=>V, (v:V)=>void, ()=>V ];
 export const create:Create = fn => {
   const { getState, setState, subscribe } = zcreate(fn);
-  const useLocalState = ()=>{
-    const [state, setLocal] = useState(getState());
-    useEffect(()=>subscribe(setLocal),[]);
-    return state;
-  }
-  return [useLocalState, setState, getState];
+  const useLocalState = () => useSyncExternalStore(subscribe, getState, getState);
+  return [useLocalState, v => setState(v, true), getState];
 }
